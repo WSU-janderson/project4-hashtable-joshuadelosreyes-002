763ae7e35868b428e2e00056161e911229ee00a4
joshuadelosreyes-002 <delosreyes.3@wright.edu>
2025-10-20 16:23:31 -0400

    Started declaring HashTable class.

--- HashTable.h
+++ HashTable.h
@@ -5,6 +5,33 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include <string>
+#include <vector>
+#include <optional>
 
+class HashTable {
+	public:
+		HashTable(size_t initCapacity = 8);
+
+		bool insert(std::string key, int value);
+		bool remove(std::string key);
+		bool contains(const std::string &key) const;
+
+		std::optional<int> get(const std::string &key) const;
+
+		int & operator[](const std::string &key);
+
+		std::vector<std::string> keys() const;
+
+		double alpha() const;
+
+		size_t capacity() const;
+		size_t size() const;
+
+		friend std::ostream & operator<<(std::ostream &os, const HashTable &hashTable);
+
+	private:
+
+};
 
 #endif
